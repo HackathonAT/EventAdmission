b4d4adf40e5e6ac23b46913ac870b597fb757aa1
Gersa Tome <Gersa.Tome@capdemooutlook.onmicrosoft.com>
2017-11-23 16:09:33 +0000

    Registration made.

---
+++ FaceAPI_Services/FaceAPI_Services/Registration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Registration
+{
+	public Registration()
+	{
+        const string personGroupId = "6368468496874";
+        const string friend1ImageDir = @"C:\Users\gtome\Downloads\gersa";
+        foreach (string imagePath in Directory.GetFiles(friend1ImageDir, "*.jpg"))
+        {
+            using (Stream s = File.OpenRead(imagePath))
+            {
+                // Detect faces in the image and add to Gersa
+                await faceServiceClient.AddPersonFaceAsync(
+                    personGroupId, friend1.PersonId, s);
+            }
+        }
+    }
+}
+
+
+
+
