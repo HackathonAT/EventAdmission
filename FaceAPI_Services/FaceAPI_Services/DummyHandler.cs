f7b1dad607fcdb505b98f91fcbd92249ff3b6cf4
Vanhee, Dree <dree.vanhee@capgemini.com>
2017-11-24 07:21:07 +0000

    I dont care

---
+++ FaceAPI_Services/FaceAPI_Services/DummyHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaceAPI_Services
+{
+    public class DummyHandler
+    {
+        public static int testc = 0;
+        public DummyHandler()
+        {
+
+        }
+
+        public void HeyArmin(object o)
+        {
+            string test = (string)o;
+        }
+
+        public string SupArmin()
+        {
+            return "Beer time!";
+        }
+    }
+}
